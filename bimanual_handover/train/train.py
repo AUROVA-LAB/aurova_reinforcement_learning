67ba5af043e36ea5af3ac9b2b807e15a45c32646
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2025-01-13 12:34:43 +0100

    Modified: increase video length

--- bimanual_handover/train/train.py
+++ bimanual_handover/train/train.py
@@ -22,7 +22,7 @@ from omni.isaac.lab.app import AppLauncher
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Train an RL agent with Stable-Baselines3.")
 parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
-parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
+parser.add_argument("--video_length", type=int, default=600, help="Length of the recorded video (in steps).")
 parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
