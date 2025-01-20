464db11d5c2a937d2b616a64660b910b8df371cd
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2025-01-20 14:09:50 +0100

    Modified: increase approx term

--- bimanual_handover/bimanual_direct_env.py
+++ bimanual_handover/bimanual_direct_env.py
@@ -592,7 +592,7 @@ class BimanualDirect(DirectRLEnv):
 
         # Compute intermediate reward terms with scaling values and boolean flags --> / (1 + 2*(hand_obj_dist_back[:,0] < hand_obj_dist[:,0]).int())
         # reward_1 = mod * rew_scale_hand_obj * torch.exp(-2*hand_obj_dist[:, 0]) / (1 + 2*(ee_pose[:, 1] - obj_pose[:, 1] < 0.0275).int()) + self.cfg.bonus_obj_reach * self.obj_reached / 5
-        reward_1 = mod * rew_scale_hand_obj * torch.exp(-2*hand_obj_dist[:, 0]) / (1 + 2*(hand_obj_dist_back[:,0] < hand_obj_dist[:,0]).int()) + self.cfg.bonus_obj_reach * self.obj_reached / 5
+        reward_1 = 1.5*mod * rew_scale_hand_obj * torch.exp(-2*hand_obj_dist[:, 0]) / (1 + 2*(hand_obj_dist_back[:,0] < hand_obj_dist[:,0]).int()) + self.cfg.bonus_obj_reach * self.obj_reached / 5
         reward_2 = mod * rew_scale_obj_target * torch.exp(-2*obj_target_dist[:, 0]) + self.cfg.bonus_obj_reach * self.obj_reached_target
 
         reward = (reward_1) * torch.logical_not(self.obj_reached) + 3*self.rew_2 * self.obj_reached + contacts_w.sum(-1) + (contacts_w.sum(-1) > 1.1).int() * 0.6
