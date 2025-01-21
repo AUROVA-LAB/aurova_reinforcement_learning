aacb12e9611723d2fee65dacc6d6a0aafb542112
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2025-01-21 13:06:40 +0100

    Modified: change entry point to PPO

--- bimanual_handover/__init__.py
+++ bimanual_handover/__init__.py
@@ -11,7 +11,7 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": BimanualDirectCfg,
-        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_sac_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:CartpolePPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
