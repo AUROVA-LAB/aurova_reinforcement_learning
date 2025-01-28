c3879b41fcedcf56e8d0d74fb75ab128ab34d441
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2025-01-28 10:46:22 +0100

    Modified: train for dual manipulation

--- bimanual_handover/train/train.py
+++ bimanual_handover/train/train.py
@@ -175,13 +175,16 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
         obs = env.reset()
         # agent = PPO.load("/workspace/isaaclab/source/logs/sb3/Isaac-UR5e-joint-reach-v0/2024-10-16_12-32-25/model_18960000_steps.zip", weights_only=True)
                              
-        action = torch.rand((env_cfg.scene.num_envs, 6+4))
-        action = torch.tensor([[0, 0, 0, 0, 0, 0, 
-                                 0.1, 0.1, 0.1]])
+        # action = torch.tensor([[0, 0, 0, 0, 0, 0, 
+        #                          -0., -0., -0.,
+        #                         0, 0, 0, 0, 0, 0,
+        #                          0.1, 0.1, 0.1]]).repeat(env_cfg.scene.num_envs, 1)
 
         # Simulate physics
         while simulation_app.is_running():
             with torch.inference_mode():
+                
+                action = torch.rand((env_cfg.scene.num_envs, 6+3 + 6 + 3))
 
                 # Step the environment
                 obs, reward, done, info = env.step(action)
