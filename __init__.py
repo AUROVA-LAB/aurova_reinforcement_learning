8a8209b0c292a852718c19de1be48427fe6e06ef
DanielFrauAlfaro <danielfraualfa@gmail.com>
2025-03-26 09:49:49 +0100

    Remove: joint space environment removed

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
 from .bimanual_handover.bimanual_direct_env import *
-from .bimanual_handover.bimanual_direct_env_joint_space import *
 from .bimanual_handover.bimanual_direct_env_cfg import *
-from .bimanual_handover.bimanual_direct_env_joint_cfg import *
 
