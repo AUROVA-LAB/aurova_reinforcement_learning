4402a8a69e933f394c34b1adc78a5957a118c0e1
DanielFrauAlfaro <danielfraualfa@gmail.com>
2025-06-03 13:28:06 +0200

    Modified: USD and URDF models with contact sensors

--- rl_manipulation/robots_cfg.py
+++ rl_manipulation/robots_cfg.py
@@ -107,7 +107,7 @@ UR5e_4f_CFG = ArticulationCfg(
 
 UR5e_3f_CFG = ArticulationCfg(
     spawn = sim_utils.UsdFileCfg(
-        usd_path="/workspace/isaaclab/source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/classic/aurova_reinforcement_learning/rl_manipulation/config/usd/ur5e_3f_ros2.usd",
+        usd_path="/workspace/isaaclab/source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/classic/aurova_reinforcement_learning/rl_manipulation/config/usd/ur5e_position_ros2_3f.usd",
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=True,
             max_depenetration_velocity=5.0,
