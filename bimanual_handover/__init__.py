bfe1cf35b2a0f35e117b37318bb798edabb972c6
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2025-10-01 14:51:53 +0200

    Delete: joint environment

--- bimanual_handover/__init__.py
+++ bimanual_handover/__init__.py
@@ -19,16 +19,3 @@ gym.register(
         "skrl_ddpg_cfg_entry_point": f"{agents.__name__}:skrl_ddpg_cfg.yaml"
     }
 )
-
-gym.register(
-    id="Isaac-Bimanual-Direct-reach-joint-v0",
-    entry_point="omni.isaac.lab_tasks.manager_based.classic.aurova_reinforcement_learning.bimanual_handover.bimanual_direct_env_joint_space:BimanualDirect",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": BimanualDirectJointCfg,
-        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:CartpolePPORunnerCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
-    }
-)
