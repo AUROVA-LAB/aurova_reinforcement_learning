e6c12917e08b6344ac262a18ddb65fd8212f8be7
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2026-02-27 15:48:11 +0100

    Fix: freezing of the object relative pose

--- rl_manipulation/rl_manipulation_direct_env.py
+++ rl_manipulation/rl_manipulation_direct_env.py
@@ -554,8 +554,8 @@ class RLManipulationDirect(DirectRLEnv):
         # Transform to the Lie algebra leveraging symmetry
         self.robot_rot_ee_pose_r_lie = self.log(self.pose_group_r)
         diff = self.diff_operator(self.target_pose_r_group, self.pose_group_r)
-        self.robot_rot_ee_pose_r_lie_rel = self.log(diff) * self.grasp_reached.unsqueeze(-1) + \
-                                           self.robot_rot_ee_pose_r_lie_rel * torch.logical_not(self.grasp_reached).unsqueeze(-1)
+        self.robot_rot_ee_pose_r_lie_rel = self.log(diff) * torch.logical_not(self.grasp_reached).unsqueeze(-1) + \
+                                           self.robot_rot_ee_pose_r_lie_rel * self.grasp_reached.unsqueeze(-1)
 
         diff_interm = self.diff_operator(self.interm_target_pose_r_group, self.pose_group_r)
         self.interm_robot_rot_ee_pose_r_lie_rel = self.log(diff_interm)
