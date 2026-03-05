39954a69d9c10d89b1ef96e134a9037be7485e4f
DanielFrauAlfaro <danielfraualfa@gmail.com>
2026-03-05 15:57:07 +0100

    Modified: only give reward for contacts if three are contacting

--- rl_manipulation/rl_manipulation_direct_env.py
+++ rl_manipulation/rl_manipulation_direct_env.py
@@ -655,7 +655,7 @@ class RLManipulationDirect(DirectRLEnv):
         # Reward for the approaching
         reward = (torch.logical_not(self.target_reached) * (self.g_action < 0.0)).float()        
         reward += (torch.logical_and(self.target_reached, torch.logical_not(self.end_reached)) * (self.g_action > 0.0)).float()
-        reward += contacts_w * self.target_reached
+        reward += is_contact * contacts_w * self.target_reached
         reward += (self.end_reached * (self.g_action < 0.0)).float()
         reward += (self.end2_reached * self.cfg.bonus_tgt_reached).float()
 
