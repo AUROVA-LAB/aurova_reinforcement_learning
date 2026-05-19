5926feb30e6bfe85f7b3cfe7f6d3b9c3a41c6079
DanielFrauAlfaro <danielfraualfa@gmail.com>
2026-05-19 16:38:17 +0200

    Add: visualiser for data

---
+++ rl_manipulation_obstacles/train/vis_data.py
@@ -0,0 +1,32 @@
+import os
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.utils.data import DataLoader, random_split
+
+from data import *
+from networks_lfd import *
+from train_utils import collate_fn
+
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+
+
+
+# =========================================================
+# TRAINING
+# =========================================================
+
+
+def vis():
+
+    dataset = HDF5LfDDataset(os.path.join(os.getcwd(), "../dataset"))
+
+
+    for i in range(len(dataset)):
+        print(i)
+
+
+if __name__ == "__main__":
+    vis()
