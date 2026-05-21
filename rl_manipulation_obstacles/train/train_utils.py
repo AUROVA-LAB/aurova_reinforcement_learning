1bd040c23539a7e1ec87f8a67f1ebde013009308
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2026-05-21 16:24:58 +0200

    Modified: point cloud and image processing for SAM

--- rl_manipulation_obstacles/train/train_utils.py
+++ rl_manipulation_obstacles/train/train_utils.py
@@ -7,6 +7,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from isaaclab.utils.math import matrix_from_quat
+
+import cv2 as cv
+
+
 
 
 def collate_fn(batch):
@@ -75,3 +80,91 @@ class Critic(DeterministicMixin, Model):
         x = F.relu(self.linear_layer_1(torch.cat([inputs["states"], inputs["taken_actions"]], dim=1)))
         x = F.relu(self.linear_layer_2(x))
         return self.linear_layer_3(x), {}
+    
+
+def depth_to_pointcloud(depth, fx, fy, cx, cy):
+    """
+    depth: (H, W)
+    returns: (N, 3) points in camera frame
+    """
+
+    device = depth.device
+
+    H, W = depth.shape
+
+    y, x = torch.meshgrid(
+        torch.arange(H, device=device),
+        torch.arange(W, device=device),
+        indexing='ij'
+    )
+
+    z = depth
+    x = (x - cx) * z / fx
+    y = (y - cy) * z / fy
+
+    points = torch.stack((x, y, z), dim=-1)
+
+    return points.reshape(-1, 3)
+
+
+
+def transform_points(points, translation, quaternion):
+    """
+    points: (N, 3)
+    translation: (3,)
+    quaternion: (4,)  [x,y,z,w]
+    """
+
+    R = matrix_from_quat(quaternion)
+
+    points_world = (R @ points.T).T + translation
+
+    return points_world
+
+def preprocess_img(img):
+    '''
+    In:
+        - img: list of images
+    '''
+
+    img = np.transpose(img, (1, 2, 0))
+
+    resized = cv.resize(img, (1024, 1024))
+
+    tensor = torch.from_numpy(resized).float().cuda()
+
+    # HWC -> BCHW
+    tensor = tensor.permute(2, 0, 1).unsqueeze(0)
+
+    # normalize to SAM expected range
+    # tensor = tensor / 255.0
+
+    # SAM normalization
+    pixel_mean = torch.tensor(
+        [123.675, 116.28, 103.53],
+        device=tensor.device
+    ).view(1, 3, 1, 1)
+
+    pixel_std = torch.tensor(
+        [58.395, 57.12, 57.375],
+        device=tensor.device
+    ).view(1, 3, 1, 1)
+
+    tensor = tensor * 255.0
+
+    tensor = (tensor - pixel_mean) / pixel_std
+
+    return tensor
+
+def preprocess_img_sam(dataset):
+
+    for i in range(len(dataset)):
+
+        cam_D = preprocess_img(dataset[i]["cam_D"])
+        cam_ext_D = preprocess_img(dataset[i]["cam_ext_D"])
+        cam_front_D = preprocess_img(dataset[i]["cam_front_D"])
+
+        dataset.set_item(i, cam_p = cam_D,
+                            cam_ext_p = cam_ext_D,
+                            cam_front_p = cam_front_D)
+
