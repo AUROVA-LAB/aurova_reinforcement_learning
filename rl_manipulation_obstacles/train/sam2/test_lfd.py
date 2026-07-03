Modified: BERT with no weights and fix centroid computation

rl_manipulation_obstacles/train/sam2/test_lfd.py
@@ -58,15 +58,15 @@ def test():
         dataset = preprocess_img_sam2(dataset)
 
     elif MODE == "pcd":
-        dataset, curr_max = preprocess_pcd(dataset, test_curr_max=1.11878)
+        dataset, curr_max = preprocess_pcd(dataset, test_curr_max=1.11878, test = True)
 
 
 
     train_ds, val_ds, test_ds = random_split(dataset, [train_size, val_size, test_size])
 
-    train_loader = DataLoader(train_ds, batch_size=32, shuffle=True, collate_fn = collate_fn)
-    val_loader = DataLoader(val_ds, batch_size=32, shuffle = True, collate_fn = collate_fn)
-    test_loader = DataLoader(test_ds, batch_size=1, shuffle = False, collate_fn = collate_fn)
+    train_loader = DataLoader(train_ds, batch_size=32, shuffle=False, collate_fn = collate_fn)
+    val_loader = DataLoader(val_ds, batch_size=32, shuffle = False, collate_fn = collate_fn)
+    test_loader = DataLoader(test_ds, batch_size=32, shuffle = False, collate_fn = collate_fn)
 
     # Get dimensions
     sample = dataset[0]
@@ -92,9 +92,14 @@ def test():
     mse_loss = 0
     mae_loss = 0
 
-    dataset.max_action = 1.56627
-    dataset.max_gripper = 1.56891
-    dataset.max_pc = 12.886685
+    dataset.max_action = 1.0
+    dataset.max_gripper = 1.0
+    dataset.max_pc = 9.827
+
+    with open("action_preprocessing.pkl","rb") as f:
+        stats = pickle.load(f)
+
+    qt = stats["qt_pc"]
 
     with torch.no_grad():
         for b in test_loader:
@@ -108,11 +113,26 @@ def test():
             pose = b["pose_seq"].to(device)
             traj = b["action"].to(device)
 
-            pred = model(pc, pose)
+            pred = model(pc)
+
+            traj = torch.tensor(qt.inverse_transform(traj.cpu().numpy()))
+            pred = torch.tensor(qt.inverse_transform(pred.cpu().numpy()))
+
+            
+
+            # print(pc)
+            # print(pose)
+            # print(traj)
+            # print(pred)
+
+            print("SL1: ", criterion(pred, traj))
+            print("MSE: ", criterion2(pred, traj))
+            print("L1: ", torch.abs(pred - traj).mean(dim=0))
 
             sl1_loss = criterion(pred, traj)
             mse_loss = criterion2(pred, traj)
             mae_loss += torch.abs(pred - traj).mean().item()
+            print("----")
 
     sl1_loss /= len(test_loader)
     mse_loss /= len(test_loader)

rl_manipulation_obstacles/train/sam2/train_lfd.py
@@ -112,12 +112,6 @@ def train():
     print("\n ------ Start training \n")
 
 
-        
-    weights = torch.tensor(
-        [[2,2,2,1,1.5,1]],
-        device=device
-    )
-
     for epoch in range(500):
 
         # ================= TRAIN =================
@@ -137,15 +131,7 @@ def train():
             pred = model(pc)
 
 
-            loss = criterion(pred, traj)*weights
-            loss = (
-                weights *
-                nn.functional.smooth_l1_loss(
-                    pred,
-                    traj,
-                    reduction='none'
-                )
-            ).mean()
+            loss = criterion(pred, traj)
 
             optimizer.zero_grad()
             loss.backward()

rl_manipulation_obstacles/train/sam2/train_utils.py
@@ -293,6 +293,7 @@ def preprocess_img_sam2(dataset):
 
 def normalize_pc(pc):
         centroid = pc.mean(dim=1, keepdim=True)
+
         pc_centered = pc - centroid
         scale = (
             torch.linalg.norm(pc_centered, dim=1)
@@ -300,7 +301,9 @@ def normalize_pc(pc):
             .values
             .unsqueeze(-1)
         )  # (B,1,1)
-        pc_norm = pc_centered / scale
+
+        pc_norm = pc_centered / scale.squeeze(-1)
+
         return pc_norm, centroid, scale
 
 def preprocess_pcd_single(pc_all, model, mode="BERT"):
@@ -351,7 +354,8 @@ def preprocess_pcd_single(pc_all, model, mode="BERT"):
     pc_xyz = pc_all[:, :3]
     pc_rgb = pc_all[:, 3:]#  if pc_all.shape[1] > 3 else np.zeros_like(pc_xyz)
 
-    pc_xyz, _, _ = normalize_pc(torch.tensor(pc_xyz))
+    pc_xyz, _, _ = normalize_pc(torch.tensor(pc_xyz).unsqueeze(0))
+
 
     if mode == "PointNet2":
         sampled_pts, sampled_idx = farthest_point_sampling(
@@ -538,8 +542,9 @@ def preprocess_pcd(dataset, mode = "BERT", test_curr_max = None, test = False):
         pos_norm = qt_pos.fit_transform(pos_list)
 
         for j in range(6):
-            actions_norm[:, j] = 2*(actions_norm[:, j]-actions_norm[:, j].min())/(actions_norm[:, j].max()-actions_norm[:, j].min())-1
-            pos_norm[:, j] = 2*(pos_norm[:, j]-pos_norm[:, j].min())/(pos_norm[:, j].max()-pos_norm[:, j].min())-1
+
+            actions_norm[:, j] = 2*(actions_norm[:, j]-stats["actions_minmax"][j][0])/(stats["actions_minmax"][j][1]-stats["actions_minmax"][j][0])-1
+            pos_norm[:, j] = 2*(pos_norm[:, j]-stats["pos_minmax"][j][0])/(stats["pos_minmax"][j][1]-stats["pos_minmax"][j][0])-1
 
         for i in range(len(dataset)):
             dataset.set_item(i, action = actions_norm[i], gripper_pose = pos_norm[i])
@@ -604,7 +609,7 @@ def preprocess_pcd(dataset, mode = "BERT", test_curr_max = None, test = False):
             "max_pc": max_pc,
             "min_pc": min_pc,
             "actions_minmax": actions_minmax,
-            "actions_minmax": actions_minmax,
+            "pos_minmax": pos_minmax,
         }
 
         with open("action_preprocessing.pkl","wb") as f:
@@ -655,7 +660,7 @@ def farthest_point_sampling(points, n_samples):
     return points[sampled_indices], sampled_indices
 
 def farthest_point_sampling_BERT(xyz, npoint):
-    xyz = torch.tensor(xyz).float().unsqueeze(0)
+    xyz = torch.tensor(xyz).float()#.unsqueeze(0)
     B, N, C = xyz.shape
 
     centroids = torch.zeros(B, npoint, dtype=torch.long)