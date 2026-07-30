f430077439bdbe31bf8a5209a986b1bf1c8ec4e3
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2026-07-30 12:20:24 +0200

    Modified: more saving frequency

--- rl_manipulation_obstacles/train/train.py
+++ rl_manipulation_obstacles/train/train.py
@@ -230,7 +230,7 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
         agent = agent.load(args_cli.checkpoint, env, print_system_info=True)
 
     # callbacks for agent
-    checkpoint_callback = CheckpointCallback(save_freq=1000, save_path=log_dir, name_prefix="model", verbose=2)
+    checkpoint_callback = CheckpointCallback(save_freq=5000, save_path=log_dir, name_prefix="model", verbose=2)
     callbacks = [checkpoint_callback, LogEveryNTimesteps(n_steps=args_cli.log_interval), WandbCallback()]
 
 
