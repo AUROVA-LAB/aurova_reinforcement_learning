252ae4d49227f8e131a67980a076d8331647fcd5
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2026-09-11 13:39:29 +0200

    debug

--- rl_manipulation_obstacles/train/sam2/train_utils.py
+++ rl_manipulation_obstacles/train/sam2/train_utils.py
@@ -913,6 +913,8 @@ def preprocess_pcd(dataset, mode = "BERT", test_curr_max = None, test = False):
 
             point_features_robot, centroid, scale = preprocess_pcd_single(pc_robot, model, mode = mode)
             point_features_object, centroid, scale = preprocess_pcd_single(pc_object, model, mode = mode)
+            print(point_features_object)
+            print(point_features_robot)
             raise
 
             
