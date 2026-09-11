cb8b5ac6f3cd976a9ef5eb1ee621c752ae1ff1cf
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2026-09-11 13:59:48 +0200

    debug

--- rl_manipulation_obstacles/train/sam2/train_utils.py
+++ rl_manipulation_obstacles/train/sam2/train_utils.py
@@ -905,6 +905,7 @@ def preprocess_pcd(dataset, mode = "BERT", test_curr_max = None, test = False):
             print(torch.isinf(point_features_robot).any())
             # print(torch.isfinite(point_features_robot).all())
             print(torch.isnan(point_features_object))
+            print(dataset[i]["diff"])
             print(torch.isinf(point_features_object).any())
             if torch.isinf(point_features_object).any().item() or torch.isnan(point_features_object).any().item() or \
                 torch.isinf(point_features_robot).any().item() or torch.isnan(point_features_robot).any().item():
