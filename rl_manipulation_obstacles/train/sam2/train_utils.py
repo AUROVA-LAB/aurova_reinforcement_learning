404e1ef9c787e90d8affc4d01bd83265e4bbea63
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2026-09-11 13:40:57 +0200

    debug

--- rl_manipulation_obstacles/train/sam2/train_utils.py
+++ rl_manipulation_obstacles/train/sam2/train_utils.py
@@ -688,18 +688,6 @@ def preprocess_pcd_single(pc_all, model, mode="BERT"):
             # point_features = dct_reducer.encode(point_features[0]).permute(1,0)
             # point_features = (point_features - torch.mean(point_features)) / torch.std(point_features)
 
-            # Check for NaNs
-            print(torch.isnan(point_features).any())
-
-            
-
-            # Check for infinities too
-            print(torch.isinf(point_features).any())
-
-            # Check for NaN OR Inf
-            print(torch.isfinite(point_features).all())
-            print("\n\n")
-
     point_features = (point_features - point_features.mean()) / (point_features.std() + 1e-8)
 
     # point_features = point_features.cpu().numpy()
@@ -913,8 +901,12 @@ def preprocess_pcd(dataset, mode = "BERT", test_curr_max = None, test = False):
 
             point_features_robot, centroid, scale = preprocess_pcd_single(pc_robot, model, mode = mode)
             point_features_object, centroid, scale = preprocess_pcd_single(pc_object, model, mode = mode)
-            print(point_features_object)
-            print(point_features_robot)
+            print(torch.isnan(point_features_robot).any())
+            print(torch.isinf(point_features_robot).any())
+            print(torch.isfinite(point_features_robot).all())
+            print(torch.isnan(point_features_object).any())
+            print(torch.isinf(point_features_object).any())
+            print(torch.isfinite(point_features_object).all())
             raise
 
             
